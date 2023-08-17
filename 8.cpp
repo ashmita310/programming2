46e73a07eb2a8cd3bc1a856f0f6e7743177b125b
ashmita310 <ashmitasaha13@gmail.com>
2023-08-18 01:54:36 +0530

    copy array

---
+++ 8.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int copy(string book[],string author[],int size)
+{
+    for(int i=0;i<size;i++)
+        author[i]=book[i];
+}
+int main()
+{
+    string book[] = {"a","b","c","d","e"};
+    int size=sizeof(book)/sizeof(book[0]);
+    string author[size];
+    copy(book,author,size);
+    cout<<"Book"<<endl;
+    for(int i=0; i<size; i++)
+        cout<<book[i]<<endl;
+    cout<<"Author"<<endl;
+    for(int i=0;i<size;i++)
+        cout<<author[i]<<endl;
+}
