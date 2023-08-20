931c3c27d68b6b251117c1de6e34e326284cca50
ashmita310 <ashmitasaha13@gmail.com>
2023-08-20 20:39:47 +0530

    product of array elements

---
+++ 12.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int product(int weight[], int size)
+{
+    int result = 1;
+    for(int i=0; i<size; i++)
+        result=result*weight[i];
+    return result;
+}
+int main()
+{
+    int weight[] = {5,1,9,7,4};
+    int size = sizeof(weight)/sizeof(weight[0]);
+    float result = product(weight, size);
+    cout<<" Product of weights : "<<result;
+}
